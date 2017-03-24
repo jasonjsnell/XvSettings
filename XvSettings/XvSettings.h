a3836d813023a49bc463acd2e0c023a380da5a3b
jasonjsnell <snell.jason@gmail>
2017-03-23 19:35:22 -0700

    Initial Commit

---
+++ XvSettings/XvSettings.h
@@ -0,0 +1,19 @@
+//
+//  XvSettings.h
+//  XvSettings
+//
+//  Created by Jason Snell on 3/23/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvSettings.
+FOUNDATION_EXPORT double XvSettingsVersionNumber;
+
+//! Project version string for XvSettings.
+FOUNDATION_EXPORT const unsigned char XvSettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvSettings/PublicHeader.h>
+
+
